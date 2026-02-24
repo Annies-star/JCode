c43f98131584d4a598ff4bc4e963cb32ff602173
P.ANNIES HEPSIBA <157796391+Annieshepsiba@users.noreply.github.com>
2026-02-25 00:38:47 +0530

    linear search

---
+++ LinearSearch.java
@@ -0,0 +1,23 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 1, 2};
+        int target = 1;
+
+        int index = linearSearch(arr, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+    
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
